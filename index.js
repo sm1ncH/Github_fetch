d531b90318358f847b159144f0361fcca1dfebe6
sm1ncH <78959537+sm1ncH@users.noreply.github.com>
2023-04-12 16:07:14 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const stats = document.querySelector('.stats-div');
+const inp_div = document.querySelector('.inp-div');
+
+button.addEventListener('click', () => {
+    const value = input.value;
+    const url = `https://api.github.com/users/${value}`;
+    fetch(url)
+        .then(Response => {
+            if(Response.ok) {
+                return Response.json();
+            } else {
+                throw new Error('Request failed!');
+            }
+        })
+        .then(data => {
+            console.log(data.avatar_url);
+            const html = `
+                <div class="stats-div">
+                    <a href="https://github.com/${data.login}"><img src="${data.avatar_url}" alt="avatar"></a>
+                    <h2>${data.name}</h2>
+                </div>
+            `;
+            stats.innerHTML = html;
+        })
+    
+});
