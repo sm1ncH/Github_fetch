495b1c246c02cd8a46240502975520c31a9eff44
sm1ncH <78959537+sm1ncH@users.noreply.github.com>
2023-04-13 21:19:43 +0200

    styleupdate

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const stats = document.querySelector('.stats-div');
 const inp_div = document.querySelector('.inp-div');
+const more_inf = document.querySelector('.more_info');
+const pop = document.querySelector('.inner');
+
 
 button.addEventListener('click', () => {
     const value = input.value;
@@ -17,12 +20,56 @@ button.addEventListener('click', () => {
         .then(data => {
             console.log(data.avatar_url);
             const html = `
-                <div class="stats-div">
-                    <a href="https://github.com/${data.login}"><img src="${data.avatar_url}" alt="avatar"></a>
+                <style>
+                .stats {
+                    widht: 300px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    flex-direction: column;
+                    justify-content: center;
+                    background-color: #fff;
+                    border-radius: 10px;
+                    padding: 20px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+                    // margin: 20px;
+                    color: black;
+                    transition: all 0.5s ease-in-out;
+                  }
+                  .stats .show{
+                    opacity: 0;
+                  }
+                  .stats{
+                    height: 150px;
+                  }
+                  a {
+                    text-decoration: none;
+                  }
+                  .stats:hover {
+                    transform: scale(1.7);
+                    // height: 270px;
+                    margin-left: -20px;
+                    margin-top: -20px;
+                    height: 200px;
+                    margin-bottom: 70px;
+                    padding: 20px auto;
+                  }
+                  .stats:hover .show{
+                    opacity: 1;
+                  }
+                </style>
+                <a href="https://github.com/${data.login}">
+                <div class="stats">
+                    <img src="${data.avatar_url}" alt="avatar">
                     <h2>${data.name}</h2>
                     <p>${data.location}</p>
                     <p>${data.bio}</p>
+                    <p class="show">${data.followers} Followers</p>
+                    <p class="show">${data.following} Following</p>
+                    <p class="show">${data.public_repos} Repos</p>
+                    <p class="show">${data.created_at} Created</p>
                 </div>
+                </a>
             `;
             stats.innerHTML = html;
         })
